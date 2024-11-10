c4712030f7f6a54ccb607f67193786682c6293a5
Wilson dos Santos Alberto <will.neto@outlook.com>
2024-11-09 22:42:08 -0300

    Add project files.

---
+++ src/CSharp.NET.ORM.EntityFrameworkCore/Data/Context/LivrariaContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CSharp.NET.ORM.EntityFrameworkCore.Data.Context
+{
+    public class LivrariaContext : DbContext
+    {
+        public LivrariaContext(DbContextOptions<LivrariaContext> dbContext) : base(dbContext)
+        {
+            /* 
+             * DbContextOptions é a classe responsável para aplicar configurações customizadas do contexto.
+             * Essas configurações são aplicadas a partir do método OnConfiguring e devem ser aplicadas através do builder.           
+            */
+        }
+
+        /*
+         * Método responsável pela configuração do DbContext.
+         * É executado antes da abertura de conexão do DbContext
+         * Se caso utilizado Injeção de Dependência, não se faz necessário a utilização do método OnConfiguring
+            - As configurações podem ser feitas através do conteiner de serviços (ServiceCollections) do Program.cs
+         */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            /*
+             * Como comentado anteriormente, a DbContextOptionsBuilder é responsável pela configuração de um DbContext.
+             * Configurações como provedor (BD), Logs, Lazy Loading, interceptores, Retry, etc.
+             */
+
+            
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
